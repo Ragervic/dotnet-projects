32013de8a60a57a52391f7dae0b0aef4950f22ca
Ragervic <141401970+Ragervic@users.noreply.github.com>
2025-06-12 08:41:06 +0300

    Estore

--- BlazorFirstApp/obj/Debug/net9.0/BlazorFirstApp.AssemblyInfo.cs
+++ BlazorFirstApp/obj/Debug/net9.0/BlazorFirstApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorFirstApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c653abd58553cb5141df3f91ca6f3fcad458702c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64892c1e1ac09ea91208146989b62ea4d424f8c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorFirstApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorFirstApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Ecommerce/Estore.CoreBusiness/PluginInterfaces/DataStore/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Estore.CoreBusiness.Models;
+
+namespace Estore.CoreBusiness.PluginInterfaces.DataStore
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts(string filter);
+        Product GetProduct(int id);
+    }
+}
